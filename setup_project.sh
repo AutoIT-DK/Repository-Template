48a0a4667ef1b99016d7e39a2c0abcecb4ba627a
mgryning <mg@autoit.dk>
2026-03-04 08:48:07 +0100

    remove setup_project.sh script

--- setup_project.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-chmod +x install_backend.sh install_frontend.sh
-
-./install_backend.sh
-./install_frontend.sh
-
-echo "✅ Backend and frontend setup scripts executed."
